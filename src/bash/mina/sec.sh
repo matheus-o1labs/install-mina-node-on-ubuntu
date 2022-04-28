dd17e5690a7183ea7d5af533e76646f1aba08c50
Matheus <matheus@o1labs.org>
2022-04-28 23:57:49 +0000

    chown

--- src/bash/mina/sec.sh
+++ src/bash/mina/sec.sh
@@ -1,3 +1,3 @@
 docker run --interactive --tty --rm --volume $(pwd)/keys:/keys minaprotocol/mina-generate-keypair:1.2.0-fe51f1e --privkey-path /keys/my-wallet
 
-chown -R matheussantanalima:matheussantanalima .
+chown -R $USER:$USER .
